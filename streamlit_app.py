968b93730778f31e1a0cc8ab46dacdfabcec5055
Matthew Hill <hillmatthew888@gmail.com>
2026-08-31 06:21:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,6 @@ if ingredients_list: ##if statement for when fruits are chosen
         st.subheader(fruit_chosen + ' Nutriton Information')
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
       
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-      
         ##Json into dataframe
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     ##output the string
