2f1d715a8858f98172ac113a7a8f4505a464a1d0
Matthew Hill <hillmatt@colostate.edu>
2026-08-31 05:03:40 -0600

    Create Streamlit app for customizing smoothies
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,60 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+#Import col
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+##select box, now deleted
+##text input box 
+
+name_on_order = title = st.text_input("Name on Smoothie")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+#Display the Fruit Options List in Your Streamlit in Snowflake (SiS) App
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+##Add a multiselect
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+
+##Display the LIST (string data type)
+if ingredients_list: ##if statement for when fruits are chosen
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+    ##convert list to string
+    ingredients_string = ''
+    ##for loop each fruit chosen 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' ' ##space between fruits chosen
+    ##output the string
+    #st.write(ingredients_string)
+
+    
+    ##Build a SQL Insert Statement & Test it
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                    values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+
+    #st.write(my_insert_stmt)
+    #st.stop()
+
+    ##Add a submit button
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+        
+    
